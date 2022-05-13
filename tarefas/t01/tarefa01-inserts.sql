d8c9ea8bd8c349c2f52fe805db8014eaf7af6b5d
LucasSilva01 <santoslucas9956@gmail.com>
2022-05-13 20:24:04 -0300

    add inserts #1

---
+++ tarefas/t01/tarefa01-inserts.sql
@@ -0,0 +1,44 @@
+INSERT INTO public.funcionario(
+	codigo, nome, sexo, dt_nasc, salario)
+	VALUES (1, 'Lucas da Silva Santos', 'M', '1972-05-15', 1543.5);
+
+INSERT INTO public.departamento(
+	descricao, cod_gerente)
+	VALUES ('Departmaneto de Finanças', 1);
+	
+INSERT INTO public.funcionario(
+	nome, sexo, dt_nasc, salario) 
+    VALUES('Oracio Silva', 'M', '1999-09-10', 2856.8);
+
+INSERT INTO public.departamento(
+	descricao, cod_gerente)
+	VALUES ('Departmaneto de TI', 2);
+	
+INSERT INTO public.funcionario(
+	nome, sexo, dt_nasc, salario) 
+    VALUES('Railma Suzana', 'F', '1982-06-09', 1789.9);
+
+INSERT INTO public.departamento(
+	descricao, cod_gerente)
+	VALUES ('Departmaneto de Feminista', 3);
+
+INSERT INTO public.funcionario(
+	nome, sexo, dt_nasc, salario) 
+    VALUES('Alexandre o Grande', 'M', '1900-05-19', 50239.1);
+
+	
+INSERT INTO public.departamento(
+	descricao, cod_gerente)
+	VALUES ('Departmaneto de Agrícola', 4);
+	
+INSERT INTO public.departamento(
+	descricao)
+	VALUES ('Departmaneto de Fantasma');
+
+INSERT INTO public.funcionario(
+	nome, sexo, dt_nasc, salario, cod_depto) 
+    VALUES('Maria Dalva', 'F', '1972-08-11', 8953.6, 3);
+	
+INSERT INTO public.funcionario(
+	nome, sexo, dt_nasc, salario, cod_depto) 
+    VALUES('Paulinha Caldeirão', 'F', '2000-06-08', 5462.2, 3);
