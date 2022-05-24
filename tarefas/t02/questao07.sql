e7c07008fbe9f1065db48082794df6249d29d402
LucasSilva01 <santoslucas9956@gmail.com>
2022-05-24 12:29:55 -0300

    questao07 #2

---
+++ tarefas/t02/questao07.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION adc_passageiro()
+RETURNS trigger AS $$
+
+BEGIN
+    UPDATE voo
+    SET num_passageiros = num_passageiros + 1;
+END;
+
+$$ LANGUAGE plpgsql;
+
+CREATE trigger update_adc_passageiros
+AFTER INSERT ON cliente_voo
+for each ROW EXECUTE FUNCTION adc_passageiro();
